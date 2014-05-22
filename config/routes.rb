d0f213aabbcb749ea212376894f7410327cffafb
chiNchiN29 <chinchin_chua29@gmail.com>
2014-05-22 14:11:36 +0800

    added help index (pages&players). updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-	get '/index' => 'pages#index'
+	root 'pages#index' # get '/' => 'pages#index'
+		get '/help' => 'pages#help'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
