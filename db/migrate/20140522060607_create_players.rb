73bdd6d6cbc21664f5a95fb12cae45282bca69e2
Jiah Margallo <jiahmargallo@gmail.com>
2014-05-22 14:07:29 +0800

    add model Player

--- db/migrate/20140522060607_create_players.rb
+++ db/migrate/20140522060607_create_players.rb
@@ -2,7 +2,10 @@ class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
       t.integer :round
-      t.string :player_name
+      t.string :p1_name
+      t.string :p2_name
+      t.integer :p1_bet
+      t.integer :p2_bet
       t.string :result
       t.integer :money
       t.string :winner
